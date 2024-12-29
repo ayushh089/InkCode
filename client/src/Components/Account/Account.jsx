db04f92c2bb749e6a1057f80456712d5c8dfed3d
aayu17 <nextcoders.89@gmail.com>
2024-12-29 23:17:53 +0530

    added leave room button

--- client/src/Components/Account/Account.jsx
+++ client/src/Components/Account/Account.jsx
@@ -17,7 +17,9 @@ const Account = () => {
 
   return (
     <div className="p-4 bg-slate-800 rounded-md shadow-md max-w-md mx-auto h-full">
-      <h2 className="text-white text-xl font-semibold mb-4">Hello, {username}</h2>
+      <h2 className="text-white text-xl font-semibold mb-4">
+        Hello, {username}
+      </h2>
       <p className="text-gray-300 mb-6">
         Share the code below with your friends to join the room.
       </p>
@@ -32,14 +34,17 @@ const Account = () => {
         </button>
       </div>
       {copySuccess && (
-        <div className="text-green-500 text-sm mt-2">Code copied to clipboard!</div>
+        <div className="text-green-500 text-sm mt-2">
+          Code copied to clipboard!
+        </div>
       )}
-      <div>
-       
-        <button className=" bg-blue-500 text-white px-4 py-2 mb-5 p-3  rounded-md fixed bottom-0 ">
-            Leave Room
+      <div className="fixed bottom-0  flex items-center justify-center">
+        <button
+          className="bg-red-500 text-white font-bold mb-5 p-3 rounded-md"
+          onClick={() => (window.location.href = "/")}
+        >
+          Leave Room
         </button>
-        
       </div>
     </div>
   );
